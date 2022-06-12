3ade2028a1c6f4369c7aedad8adaaa3c5868f613
zej <zej@DESKTOP-F1ANRSH>
2022-06-13 00:03:01 +0800

    增加SignalR

---
+++ AdminBlog.SignalRApplication/SignalRHub.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace AdminBlog.SignalRApplication
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class SignalRHub : Hub
+    {
+        /// <summary>
+        /// 客户连接成功时触发
+        /// </summary>
+        /// <returns></returns>
+        public override async Task OnConnectedAsync()
+        {
+            var cid = Context.ConnectionId;
+
+            //根据id获取指定客户端
+            var client = Clients.Client(cid);
+
+            //向指定用户发送消息
+            await client.SendAsync("Self", cid);
+
+            //像所有用户发送消息
+            await Clients.All.SendAsync("AddMsg", $"{cid}加入了聊天室");
+        }
+    }
+}
